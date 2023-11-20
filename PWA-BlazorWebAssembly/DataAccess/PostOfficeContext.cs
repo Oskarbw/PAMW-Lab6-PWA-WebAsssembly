6d5300a2c4d646b7cd12c6b32cdc53b4c5e7df06
Oskar Biwejnis <01169534@pw.edu.pl>
2023-11-20 14:12:52 +0100

    Upload files

---
+++ PWA-BlazorWebAssembly/DataAccess/PostOfficeContext.cs
@@ -0,0 +1,53 @@
+﻿using PWA_BlazorWebAssembly.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace PWA_BlazorWebAssembly.DataAccess
+{
+    public class PostOfficeContext : DbContext
+    {
+        public DbSet<Parcel> Parcels { get; set; }
+
+        public PostOfficeContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=.\\DbPostOffice");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Parcel>(eb =>
+            {
+                eb.HasKey(p => p.Id);
+                eb.Property(p => p.Sender).HasColumnType("varchar(50)");
+                eb.Property(p => p.Receiver).HasColumnType("varchar(50)").IsRequired();
+                eb.HasData(new List<Parcel>
+                {
+                    new Parcel
+                    {
+                        Id = 1,
+                        Sender = "Bob",
+                        Receiver = "Ross",
+                        Weight = 14
+                    },
+                    new Parcel
+                    {
+                        Id = 2,
+                        Sender = "Miley",
+                        Receiver = "Karen",
+                        Weight = 17
+                    }
+                }
+                );
+            });
+
+        }
+
+    }
+}
---
+++ PWA-BlazorWebAssembly/Models/Parcel.cs
@@ -0,0 +1,10 @@
+﻿namespace PWA_BlazorWebAssembly.Models
+{
+    public class Parcel
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public int Weight { get; set; }
+    }
+}
